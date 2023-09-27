5e45535e86d07c2252dfdb24c061a2631b9ca441
Muhammadali06dev <muhammadalideveloper2006@gmail.com>
2023-09-27 15:51:49 +0500

    complete project

---
+++ src/data/Data.js
@@ -0,0 +1,17 @@
+export const userList = [
+   {
+      id: 1,
+      name: "Muhammadali",
+      email: "muhammadali@gmail.com"
+   },
+   {
+      id: 2,
+      name: "Anvar",
+      email: "anvar@gmail.com"
+   },
+   {
+      id: 3,
+      name: "Doniyor",
+      email: "doniyor@gmail.com"
+   },
+]
